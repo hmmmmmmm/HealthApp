f76129cc897feeeeff672431e1f3bf8882cbc0dd
Eilidh Martin <hmmmmmmm@users.noreply.github.com>
2021-04-29 23:39:36 +0100

    Removing old admin.php and updating SQL script

--- health_app.sql
+++ health_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2021 at 03:14 PM
+-- Generation Time: Apr 30, 2021 at 12:38 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -24,6 +24,77 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `exercise_data`
+--
+
+CREATE TABLE `exercise_data` (
+  `user_id` int(11) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `ename` text NOT NULL,
+  `etime` text NOT NULL,
+  `enotes` text NOT NULL,
+  `exercise_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `exercise_data`
+--
+
+INSERT INTO `exercise_data` (`user_id`, `timestamp`, `ename`, `etime`, `enotes`, `exercise_id`) VALUES
+(2, '2021-04-29 22:17:37', 'Running', '1 hour', 'Ran for an hour', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `health_data`
+--
+
+CREATE TABLE `health_data` (
+  `user_id` int(11) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `heartrate` int(3) NOT NULL,
+  `bodtemp` int(3) NOT NULL,
+  `blpressure` text NOT NULL,
+  `bloxygen` int(3) NOT NULL,
+  `breathrate` int(3) NOT NULL,
+  `ecgdet` int(4) NOT NULL,
+  `checkin_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `health_data`
+--
+
+INSERT INTO `health_data` (`user_id`, `timestamp`, `heartrate`, `bodtemp`, `blpressure`, `bloxygen`, `breathrate`, `ecgdet`, `checkin_id`) VALUES
+(0, '2021-04-29 21:20:19', 0, 2, '3', 4, 5, 6, 1),
+(2, '2021-04-29 21:31:11', 10, 10, '10', 10, 10, 10, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reminders`
+--
+
+CREATE TABLE `reminders` (
+  `user_id` int(11) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `reminderdate` datetime NOT NULL,
+  `reminderdetails` text NOT NULL,
+  `reminder_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reminders`
+--
+
+INSERT INTO `reminders` (`user_id`, `timestamp`, `reminderdate`, `reminderdetails`, `reminder_id`) VALUES
+(2, '2021-04-29 22:07:22', '2021-04-30 00:00:00', 'test', 1),
+(2, '2021-04-29 22:08:52', '2021-05-01 00:00:00', 'test 2', 2),
+(2, '2021-04-29 22:14:35', '2021-05-02 03:05:00', 'fds', 3);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -33,6 +104,18 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `admin`) VALUES
+(2, 'eilidh', '$2y$10$fVbYT0eHB6XInYb9vBE6Ee9KRllLWr4Bx04ooBDz.0OSQXpbW67Ei', 1),
+(3, 'p', '$2y$10$XrTMrmi4EM0FlAyvfmH3oeyLi.Z7maIfv.cTw.mQIg3q0tpHJ/Sze', 0),
+(4, 'riderlessseven', '$2y$10$G/ZmNFcrLAAFmJ8IBGxZj.uhFrFdqmxD9omdSLJb28EFIKPIJ6s6e', 0),
+(10, 'me', '$2y$10$RhjgiPa901UNoBrW0jjBEOu0yVuaNvwUjdDXr.eOL8x.pCATTwCVi', 0),
+(11, 'caa', '$2y$10$jFdVwgmtScfIkBXw1qssde42.RyjI2Iqms.5NN0rsif/T.H6ptwx.', 0),
+(12, 'a', '$2y$10$xisDaavMntQ2TrHQyzC4ZuiVpKFfakMgkgDUFFtBKTrBs5e5FaTLa', 0);
 
 -- --------------------------------------------------------
 
@@ -48,8 +131,38 @@ CREATE TABLE `user_details` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `user_details`
+--
+
+INSERT INTO `user_details` (`user_id`, `first_names`, `surname`, `date_of_birth`) VALUES
+(2, 'Eilidh', 'Martin', '1993-12-31'),
+(3, 'Peter', 'Majek', '1990-01-01'),
+(4, 'Kevin', 'Urama', '1994-06-23'),
+(10, '', '', '0000-00-00'),
+(11, '', '', '0000-00-00'),
+(12, '', '', '0000-00-00');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `exercise_data`
+--
+ALTER TABLE `exercise_data`
+  ADD PRIMARY KEY (`exercise_id`);
+
+--
+-- Indexes for table `health_data`
+--
+ALTER TABLE `health_data`
+  ADD PRIMARY KEY (`checkin_id`);
+
+--
+-- Indexes for table `reminders`
+--
+ALTER TABLE `reminders`
+  ADD PRIMARY KEY (`reminder_id`);
 
 --
 -- Indexes for table `users`
@@ -70,10 +183,28 @@ ALTER TABLE `user_details`
 --
 
 --
+-- AUTO_INCREMENT for table `exercise_data`
+--
+ALTER TABLE `exercise_data`
+  MODIFY `exercise_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `health_data`
+--
+ALTER TABLE `health_data`
+  MODIFY `checkin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `reminders`
+--
+ALTER TABLE `reminders`
+  MODIFY `reminder_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
